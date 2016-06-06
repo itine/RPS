62fbb403ac5d514a2efe8b5a19ef78ad0edc08ed
itine <alexeevsergey888@gmail.com>
2016-06-06 23:39:18 +0300

    transaction

---
+++ Labs/Transaction/transact-ejb/build/generated-sources/ap-source-output/model1/Client_.java
@@ -0,0 +1,15 @@
+package model1;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-06-06T23:23:56")
+@StaticMetamodel(Client.class)
+public class Client_ { 
+
+    public static volatile SingularAttribute<Client, Integer> idClient;
+    public static volatile SingularAttribute<Client, String> surname;
+    public static volatile SingularAttribute<Client, String> name;
+
+}
